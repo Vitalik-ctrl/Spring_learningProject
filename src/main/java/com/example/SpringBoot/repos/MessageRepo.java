59013781615640b4e5ede6ccddb847b75c010303
Vitalik-ctrl <vitvarganik@gmail.com>
2020-02-28 23:11:21 +0200

    Spring lesson №1, with PostgreSQL

---
+++ src/main/java/com/example/SpringBoot/repos/MessageRepo.java
@@ -0,0 +1,12 @@
+package com.example.SpringBoot.repos;
+
+import com.example.SpringBoot.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Long> {
+
+    List<Message> findByTag(String tag);
+
+}
